942ed54a2ca096c91885bc6ee052c7c1df9a7d4f
ennovatenow <anish.thomas@evolvussolutions.com>
2019-02-27 16:02:17 +0530

    corrected an sql error

--- cif-activity-based-template/containers/validate-essence-cif/validate-essence-cif.sql
+++ cif-activity-based-template/containers/validate-essence-cif/validate-essence-cif.sql
@@ -1,5 +1,5 @@
 drop table o_essence_cif_validation;
-create table o_essence_cif_validation;
+create table o_essence_cif_validation as
 select 'NAME_TOO_LONG' as t_name, count(1) row_num from cu1_o_table where length(cust_first_name) > 255
 union
 select 'MIDDLE_NAME_MISSING' as t_name, count(1) row_num from cu1_o_table where cust_middle_name is null;
