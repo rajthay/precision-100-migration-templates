151e44418f83cd4949e3a603a902df47ddab2813
ennovatenow <anish.thomas@evolvussolutions.com>
2019-03-15 08:11:13 +0530

    completed extract script

---
+++ mixed-eq3-essence/containers/extract-eq3/extract-customer-casa.sh
@@ -0,0 +1,21 @@
+echo "Extracting table GFPF"
+cp $CONTAINER_FOLDER/$CONTAINER/GFPF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table SCPF"
+cp $CONTAINER_FOLDER/$CONTAINER/SCPF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table C8PF"
+cp $CONTAINER_FOLDER/$CONTAINER/C8PF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table S5PF"
+cp $CONTAINER_FOLDER/$CONTAINER/S5PF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table C5PF"
+cp $CONTAINER_FOLDER/$CONTAINER/C5PF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table SX20LF"
+cp $CONTAINER_FOLDER/$CONTAINER/SX20LF.dat $SQLLDR_INPUT
+sleep 2
+echo "Extracting table SVPF"
+cp $CONTAINER_FOLDER/$CONTAINER/SVPF.dat $SQLLDR_INPUT
+sleep 2
